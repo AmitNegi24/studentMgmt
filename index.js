1419b2d48edb6debfc68889d859c2fa0466125c2
Amit Negi <mramitnegig1999@gmail.com>
2022-11-15 17:29:25 +0530

    backend with axios

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const route = require("../studentMgmt/route")
 const  mongoose  = require("mongoose");
 const app = express();
 
+var cors = require('cors');
+app.use(cors())
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true })); 
 
@@ -17,6 +19,6 @@ mongoose.connect("mongodb+srv://Anjali-11:Krishna@cluster0.hhecqj7.mongodb.net/S
 app.use('/', route)
 
 
-app.listen(process.env.PORT || 3000, function () {
-    console.log('Express app running on port ' + (process.env.PORT || 3000))
+app.listen(process.env.PORT || 3001, function () {
+    console.log('Express app running on port ' + (process.env.PORT || 3001))
 });
